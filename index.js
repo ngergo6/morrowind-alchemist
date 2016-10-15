6fee5a344f5e924d0ec9274eb120c170205c1669
ngergo6 <n.gergo6@outlook.com>
2016-10-15 14:27:04 +0200

    fixed port

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var static = require('node-static');
 
 var file = new static.Server('./public', { indexFile: "index.html" });
 
+var port = process.env.PORT || 5000;
+
 console.log("starting server");
 
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
         file.serve(request, response);
     }).resume();
-}).listen(8080);
+}).listen(port);
