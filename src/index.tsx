3baa05ba5d35b34f30f037fefdcf681017350654
ngergo6 <n.gergo6@outlook.com>
2016-10-15 18:41:42 +0200

    hashHistory instead of html5

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 
 import { App } from "./app";
 import { IngridientsPage } from "./ingridients-page";
 import { EffectsPage } from "./effects-page";
 
 ReactDOM.render(
-    <Router history={browserHistory}>
+    <Router history={hashHistory}>
         <Route path="/" component={App}>
             <IndexRoute component={IngridientsPage} />
             <Route path="ingridients" component={IngridientsPage} />
