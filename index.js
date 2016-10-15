6e077324d96a9736e8168712074951d334fe5345
ngergo6 <n.gergo6@outlook.com>
2016-10-15 14:18:02 +0200

    set up index file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var static = require('node-static');
 
-var file = new static.Server('./public');
+var file = new static.Server('./public', { indexFile: "index.html" });
  
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
