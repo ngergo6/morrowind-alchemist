906686496057828cada425eaeabea07c68eed636
ngergo6 <n.gergo6@outlook.com>
2016-10-15 14:21:12 +0200

    logging server start to test

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var static = require('node-static');
 
 var file = new static.Server('./public', { indexFile: "index.html" });
- 
+
+console.log("starting server");
+
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
         file.serve(request, response);
